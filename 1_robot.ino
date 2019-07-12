3d2a891da6a4d3b08f3d11f9aec5fad857ce8a51
GoTronicSAV <52817715+gotronicsav@users.noreply.github.com>
2019-07-12 10:51:26 +0200

    Add files via upload

---
+++ 1_robot.ino
@@ -0,0 +1,141 @@
+#include <OneSheeld.h>
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_SHIELD
+#define INCLUDE_KEYPAD_SHIELD
+
+#include <Servo.h>
+
+Servo base;
+Servo epaule;
+Servo coude;
+Servo poignet;
+Servo rotapince;
+Servo ouvrepince;
+
+int base_val = 90;
+int epaule_val = 90;
+int coude_val = 90;
+int poignet_val = 90;
+int rotapince_val = 90;
+int ouvrepince_val = 130;
+
+int vitesse = 1;
+
+
+void setup() {
+
+  base.attach(6);
+  epaule.attach(3);
+  coude.attach(9);
+  poignet.attach(5);
+  rotapince.attach(11);
+  ouvrepince.attach(10);
+
+  OneSheeld.begin();
+  //Serial.begin(9600);
+
+  base.write(base_val);
+  epaule.write(epaule_val);
+  coude.write(coude_val);
+  poignet.write(poignet_val);
+  rotapince.write(rotapince_val);
+  ouvrepince.write(ouvrepince_val);
+
+}
+
+void loop() {
+
+  if (Keypad.isRowPressed(0) && Keypad.isColumnPressed(0)) {
+    vitesse = 1;
+    else if (Keypad.isRowPressed(0) && Keypad.isColumnPressed(2)) {
+      vitesse = 3;
+      else if (Keypad.isRowPressed(1) && Keypad.isColumnPressed(1)) {
+        vitesse = 5;
+        else vitesse = vitesse;
+
+        if (GamePad.isLeftPressed()) {
+          base_val += vitesse;
+          if (base_val >= 170) base_val = 170;
+          base.write(base_val);
+        }
+
+        if (GamePad.isRightPressed()) {
+          base_val -= vitesse;
+          if (base_val <= 10) base_val = 10;
+          base.write(base_val);
+        }
+
+
+
+        if (GamePad.isUpPressed()) {
+          epaule_val -= vitesse;
+          if (epaule_val <= 10) epaule_val = 10;
+          epaule.write(epaule_val);
+        }
+
+        if (GamePad.isDownPressed()) {
+          epaule_val += vitesse;
+          if (epaule_val >= 170) epaule_val = 170;
+          epaule.write(epaule_val);
+        }
+
+
+
+        if (GamePad.isOrangePressed()) {
+          coude_val -= vitesse;
+          if (coude_val <= 10) coude_val = 10;
+          coude.write(coude_val);
+        }
+
+        if (GamePad.isGreenPressed()) {
+          coude_val += vitesse;
+          if (coude_val >= 170) coude_val = 170;
+          coude.write(coude_val);
+        }
+
+
+
+        if (GamePad.isBluePressed()) {
+          poignet_val += vitesse;
+          if (poignet_val >= 170) poignet_val = 170;
+          poignet.write(poignet_val);
+        }
+
+        if (GamePad.isRedPressed()) {
+          poignet_val -= vitesse;
+          if (poignet_val <= 10) poignet_val = 10;
+          poignet.write(poignet_val);
+        }
+
+
+
+        if (Keypad.isRowPressed(3) && Keypad.isColumnPressed(0)) { //touche *
+          rotapince_val += vitesse;
+          if (rotapince_val >= 170) rotapince_val = 170;
+          rotapince.write(rotapince_val);
+        }
+
+        if (Keypad.isRowPressed(3) && Keypad.isColumnPressed(2)) { //touche #
+          rotapince_val -= vitesse;
+          if (rotapince_val <= 10) rotapince_val = 10;
+          rotapince.write(rotapince_val);
+        }
+
+
+
+        if (Keypad.isRowPressed(0) && Keypad.isColumnPressed(3)) { //touche A
+          ouvrepince_val += vitesse;
+          if (ouvrepince_val >= 170) ouvrepince_val = 170;
+          ouvrepince.write(ouvrepince_val);
+        }
+
+        if (Keypad.isRowPressed(1) && Keypad.isColumnPressed(3)) { //touche B
+          ouvrepince_val -= vitesse;
+          if (ouvrepince_val <= 100) ouvrepince_val = 100;
+          ouvrepince.write(ouvrepince_val);
+        }
+
+
+        delay(15);
+      }
+
---
+++ 1_ultrason.ino
@@ -0,0 +1,109 @@
+#include <OneSheeld.h>
+#define CUSTOM_SETTINGS
+#define INCLUDE_MUSIC_PLAYER_SHIELD
+#define INCLUDE_KEYPAD_SHIELD
+#define INCLUDE_NOTIFICATION_SHIELD
+
+#define ECHO 7
+#define TRIG 8
+#define Led 12
+
+int detection = 20; //100
+int codeChiffre = 0;
+byte actif = 0;
+long distance;
+long duree;
+boolean notif = false;
+
+void setup() {
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(Led, OUTPUT);
+
+  OneSheeld.begin();
+}
+
+void loop() {
+  activation();
+  if (actif == 1) {
+    condition();
+    delay(100);
+  } else {}
+}
+
+void condition() {
+
+  //Mesure du capteur
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  duree = pulseIn(ECHO, HIGH);
+
+  distance = duree / 58; //norme constructeur
+
+  if (distance < detection) {
+    MusicPlayer.play(); //on enclenche l'alarme
+    delay(15000);
+    MusicPlayer.stop();
+  }
+
+}
+
+void activation() {
+  //vérification du code : 08110
+  if (codeChiffre == 0 && toucheZero()) codeChiffre++;
+  else if (codeChiffre == 1 && toucheHuit()) codeChiffre++;
+  else if (codeChiffre == 2 && toucheUn()) codeChiffre++;
+  else if (codeChiffre == 3 && toucheUn()) codeChiffre++;
+  else if (codeChiffre == 4 && toucheZero()) {
+    codeChiffre++;
+    actif = !actif;
+    if (actif == 1) {
+      Notification.notifyPhone("Système Activé"); //envoie une notification au téléphone
+      delay(100);
+      digitalWrite(Led, HIGH); //on allume la LED
+      delay(100);
+    } else {
+      Notification.notifyPhone("Système Désactivé"); //envoie une notification au téléphone
+      delay(100);
+      digitalWrite(Led, LOW); //on allume la LED
+      delay(100);
+      MusicPlayer.stop();
+      delay(100);
+    }
+  }
+  else if ((Keypad.isAnyRowPressed() || Keypad.isAnyColumnPressed()) && !toucheZero() && !toucheUn() && !toucheHuit()) {
+    codeChiffre = 0;
+  }
+  if (codeChiffre == 5) codeChiffre = 0;
+}
+
+//fonctions pour l'utilisation des touches du clavier
+boolean toucheZero() {
+  return Keypad.isRowPressed(3) && Keypad.isColumnPressed(1); //touche 0
+}
+boolean toucheUn() {
+  return Keypad.isRowPressed(0) && Keypad.isColumnPressed(0); //touche 1
+}
+boolean toucheHuit() {
+  return Keypad.isRowPressed(2) && Keypad.isColumnPressed(1); //touche 8
+}
+
+/*
+  boolean toucheDeux(){return Keypad.isRowPressed(0) && Keypad.isColumnPressed(1);} //touche 2
+  boolean toucheTrois(){return Keypad.isRowPressed(0) && Keypad.isColumnPressed(2);} //touche 3
+  boolean toucheQuatre(){return Keypad.isRowPressed(1) && Keypad.isColumnPressed(0);} //touche 4
+  boolean toucheCinq(){return Keypad.isRowPressed(1) && Keypad.isColumnPressed(1);} //touche 5
+  boolean toucheSix(){return Keypad.isRowPressed(1) && Keypad.isColumnPressed(2);} //touche 6
+  boolean toucheSept(){return Keypad.isRowPressed(2) && Keypad.isColumnPressed(0);} //touche 7
+  boolean toucheNeuf(){return Keypad.isRowPressed(2) && Keypad.isColumnPressed(2);} //touche 9
+  boolean toucheA(){return Keypad.isRowPressed(0) && Keypad.isColumnPressed(3);} //touche A
+  boolean toucheB(){return Keypad.isRowPressed(1) && Keypad.isColumnPressed(3);} //touche B
+  boolean toucheC(){return Keypad.isRowPressed(2) && Keypad.isColumnPressed(3);} //touche C
+  boolean toucheD(){return Keypad.isRowPressed(3) && Keypad.isColumnPressed(3);} //touche D
+  boolean toucheEtoile(){return Keypad.isRowPressed(3) && Keypad.isColumnPressed(0);} //touche *
+  boolean toucheDiese(){return Keypad.isRowPressed(3) && Keypad.isColumnPressed(2);} //touche #
+*/
